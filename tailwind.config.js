4786b43035b99432402c28499cfd05d20be02801
Andreas Pandzic <andreas.pandzic@devoteam.com>
2022-11-17 09:30:35 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        md: "769px",
+        // => @media (min-width: 769px) { ... }
+
+        lg: "1201px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "2560px",
+        // => @media (min-width: 1280px) { ... }
+      },
+    },
   },
   plugins: [],
-}
+};
